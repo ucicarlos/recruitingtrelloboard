5cf56cb2b86ce6020ec0a01bcf04ae80d0842cb5
Diogenes <diogenes.hern@gmail.com>
2018-04-19 18:37:04 -0400

    jop position view testing results

--- public/js/jop_position.js
+++ public/js/jop_position.js
@@ -1,36 +1,35 @@
 /* global TrelloPowerUp */
 var t = TrelloPowerUp.iframe();
-// var data = null;
-// var xhr = new XMLHttpRequest();
+var data = null;
+var xhr = new XMLHttpRequest();
 // you can access arguments passed to your iframe like so
 // var num = t.arg('text');
 
-window.contact.addEventListener('submit', function(event){
+window.contact.addEventListener('submit', function (event) {
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-//   t.board('id', 'name')
-//     .then(function (board) {
-//       let comments = window.comments.value;
-//       let title = window.fullName.value + " | " + window.email.value + " | " + window.phone.value;
-//
-//       xhr.addEventListener("readystatechange", function () {
-//         if (this.readyState === this.DONE) {
-//           console.log(this.responseText);
-//           t.closeModal();
-//         }
-//       });
-//       xhr.open("POST", "https://api.trello.com/1/lists?name="+ title + "&idBoard=" + board.id + "&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
-//       xhr.send(data);
-//     });
+  let idList = window.recruit.id;
+  let date = window.dueDate.value;
+  let description = "Job Assigned: " + window.jobPosition.value + ", Company: " + window.company.value;
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", "https://api.trello.com/1/cards?name=Job%20Position&desc=" + description + "&pos=bottom&due=" + date + "&idList=" + idList + "&keepFromSource=all&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
+
+  xhr.send(data);
+
 });
 
-t.render(function(){
+t.render(function () {
   // this function we be called once on initial load
   // and then called each time something changes that
   // you might want to react to, such as new data being
   // stored with t.set()
   return t.lists('all')
-    .then(function(lists){
+    .then(function (lists) {
       let select = window.recruit;
       lists.forEach(function (recruit) {
         let option = document.createElement('option');
@@ -38,8 +37,5 @@ t.render(function(){
         option.innerHTML = recruit.name.split(' | ')[0];
         select.appendChild(option);
       });
-
-      console.log(window.recruit);
-      console.log(lists);
     })
 });
