590d89bb9e7e1f548cd78f54ba11bba5b4e38607
Diogenes <diogenes.hern@gmail.com>
2018-04-19 17:20:43 -0400

    new recruit view testing

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,5 +1,6 @@
 /* global TrelloPowerUp */
 var t = TrelloPowerUp.iframe();
+var request = require("request");
 // you can access arguments passed to your iframe like so
 // var num = t.arg('text');
 
@@ -16,6 +17,20 @@ window.contact.addEventListener('submit', function(event){
       let comments = window.comments.value;
       let title = name + " | " + email + " | " + phone;
       console.log(title);
+
+      var options = { method: 'POST',
+        url: 'https://api.trello.com/1/lists',
+        qs:
+          { name: title,
+            idBoard: board.id,
+            key: '805550e507939ed01e3dd28d0d55f61a',
+            token: 'f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd' } };
+
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        t.closeModal();
+        console.log(body);
+      });
     });
 
   // return t.set('card', 'shared', 'estimate', window.estimateSize.value)
