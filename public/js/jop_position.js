3d0672556e3031a3d3dcdf914b69bdef4d4ed2d8
Diogenes <diogenes.hern@gmail.com>
2018-04-20 19:59:19 -0400

    fixing errors

--- public/js/jop_position.js
+++ public/js/jop_position.js
@@ -8,47 +8,28 @@ var xhr = new XMLHttpRequest();
 window.contact.addEventListener('submit', function (event) {
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  var Promise = TrelloPowerUp.Promise;
-  var t = TrelloPowerUp.iframe();
+  t.get('member', 'private', 'token')
+  // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
+  // use t.storeSecret('key', 'value') and t.loadSecret('key')
+    .then(function(token){
+      if(token){
+        t.board('id', 'name')
+          .then(function (board) {
+            let title = window.jobPosition.value + " | " + window.dueDate.value + " | " + window.company.value;
 
-  var apiKey = '805550e507939ed01e3dd28d0d55f61a'; // Passed in as an argument to our iframe
-
-  var trelloAuthUrl = `https://trello.com/1/authorize?expiration=1hour&name=Example%20Trello%20Power-Up&scope=read&key=${apiKey}&callback_method=fragment&return_url=${window.location.origin}%2Fauth-success.html`;
-
-  var tokenLooksValid = function (token) {
-    // If this returns false, the Promise won't resolve.
-    return /^[0-9a-f]{64}$/.test(token);
-  }
-
-  t.authorize(trelloAuthUrl, {height: 680, width: 580, validToken: tokenLooksValid})
-    .then(function (token) {
-      // store the token in Trello private Power-Up storage
-      return t.set('member', 'private', 'token', token)
+            xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === this.DONE) {
+                t.closeModal();
+              }
+            });
+            xhr.open("POST", "https://api.trello.com/1/lists?name="+ title + "&idBoard=" + board.id + "&key=805550e507939ed01e3dd28d0d55f61a&token="+token);
+            xhr.send(data);
+          });
+      }
     })
-    .then(function () {
-      t.get('member', 'private', 'token')
-      // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
-      // use t.storeSecret('key', 'value') and t.loadSecret('key')
-        .then(function (token) {
-          if (token) {
-            t.board('id', 'name')
-              .then(function (board) {
-                let title = window.jobPosition.value + " | " + window.dueDate.value + " | " + window.company.value;
-
-                xhr.addEventListener("readystatechange", function () {
-                  if (this.readyState === this.DONE) {
-                    t.closeModal();
-                  }
-                });
-                xhr.open("POST", "https://api.trello.com/1/lists?name=" + title + "&idBoard=" + board.id + "&key=805550e507939ed01e3dd28d0d55f61a&token=" + token);
-                xhr.send(data);
-              });
-          }
-        })
-    });
-
 
 });
+
 
 
 t.render(function () {
