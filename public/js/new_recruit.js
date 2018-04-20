abe4351f0ff58857bcc27648bd1f4ac5c9aeb318
Diogenes <diogenes.hern@gmail.com>
2018-04-20 19:06:22 -0400

    refactoring auth

--- public/js/new_recruit.js
+++ public/js/new_recruit.js
@@ -5,23 +5,30 @@ var xhr = new XMLHttpRequest();
 // you can access arguments passed to your iframe like so
 // var num = t.arg('text');
 
-window.contact.addEventListener('submit', function(event){
+window.contact.addEventListener('submit', function (event) {
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  let idList = window.jobPosition.value;
-  let description = window.fullName.value + ", Phone: " + window.phone.value + ", Email: " + window.email.value + ", Comments: " + window.comments.value;
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      t.closeModal();
-    }
-  });
+  t.get('member', 'private', 'token')
+  // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
+  // use t.storeSecret('key', 'value') and t.loadSecret('key')
+    .then(function (token) {
+      if (token) {
+        let idList = window.jobPosition.value;
+        let description = window.fullName.value + ", Phone: " + window.phone.value + ", Email: " + window.email.value + ", Comments: " + window.comments.value;
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === this.DONE) {
+            t.closeModal();
+          }
+        });
 
-  xhr.open("POST", "https://api.trello.com/1/cards?name=" + window.fullName.value + "&desc=" + description + "&pos=bottom&idList=" + idList + "&keepFromSource=all&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
+        xhr.open("POST", "https://api.trello.com/1/cards?name=" + window.fullName.value + "&desc=" + description + "&pos=bottom&idList=" + idList + "&keepFromSource=all&key=805550e507939ed01e3dd28d0d55f61a&token=" + token);
 
-  xhr.send(data);
+        xhr.send(data);
+      }
+    });
 });
 
-t.render(function(){
+t.render(function () {
   // this function we be called once on initial load
   // and then called each time something changes that
   // you might want to react to, such as new data being
