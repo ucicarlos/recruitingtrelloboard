e12b606b60d8776a19cebff8348c5ed472c347b4
Diogenes <diogenes.hern@gmail.com>
2018-04-20 10:46:59 -0400

    refactoring job position and new recruit

--- public/js/jop_position.js
+++ public/js/jop_position.js
@@ -8,35 +8,26 @@ var xhr = new XMLHttpRequest();
 window.contact.addEventListener('submit', function (event) {
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  let idList = window.recruit.value;
-  let date = window.dueDate.value;
-  let description = "Job Assigned: " + window.jobPosition.value + ", Company: " + window.company.value;
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-      t.closeModal();
-    }
-  });
+  t.board('id', 'name')
+    .then(function (board) {
+      let comments = window.comments.value;
+      let title = window.jobPosition.value + " | " + window.dueDate.value + " | " + window.company.value;
 
-  xhr.open("POST", "https://api.trello.com/1/cards?name=Job%20Position&desc=" + description + "&pos=bottom&due=" + date + "&idList=" + idList + "&keepFromSource=all&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
-
-  xhr.send(data);
-
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+          t.closeModal();
+        }
+      });
+      xhr.open("POST", "https://api.trello.com/1/lists?name="+ title + "&idBoard=" + board.id + "&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
+      xhr.send(data);
+    });
 });
+
+
 
 t.render(function () {
   // this function we be called once on initial load
   // and then called each time something changes that
   // you might want to react to, such as new data being
   // stored with t.set()
-  return t.lists('all')
-    .then(function (lists) {
-      let select = window.recruit;
-      lists.forEach(function (recruit) {
-        let option = document.createElement('option');
-        option.value = recruit.id;
-        option.innerHTML = recruit.name.split(' | ')[0];
-        select.appendChild(option);
-      });
-    })
 });
--- public/js/new_recruit.js
+++ public/js/new_recruit.js
@@ -8,20 +8,17 @@ var xhr = new XMLHttpRequest();
 window.contact.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  t.board('id', 'name')
-    .then(function (board) {
-      let comments = window.comments.value;
-      let title = window.fullName.value + " | " + window.email.value + " | " + window.phone.value;
+  let idList = window.jobPosition.value;
+  let description = window.fullName.value + ", Phone: " + window.phone.value + ", Email: " + window.email.value + ", Comments: " + window.comments.value;
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      t.closeModal();
+    }
+  });
 
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-          console.log(this.responseText);
-          t.closeModal();
-        }
-      });
-      xhr.open("POST", "https://api.trello.com/1/lists?name="+ title + "&idBoard=" + board.id + "&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
-      xhr.send(data);
-    });
+  xhr.open("POST", "https://api.trello.com/1/cards?name=" + window.fullName.value + "&desc=" + description + "&pos=bottom&idList=" + idList + "&keepFromSource=all&key=805550e507939ed01e3dd28d0d55f61a&token=f1635aff34517609c4f7c1ca29c2c75116fb56282fd9c3ea35028dbbf9d1e4bd");
+
+  xhr.send(data);
 });
 
 t.render(function(){
@@ -29,4 +26,14 @@ t.render(function(){
   // and then called each time something changes that
   // you might want to react to, such as new data being
   // stored with t.set()
+  return t.lists('all')
+    .then(function (lists) {
+      let select = window.jobPosition;
+      lists.forEach(function (job) {
+        let option = document.createElement('option');
+        option.value = job.id;
+        option.innerHTML = job.name.split(' | ')[0];
+        select.appendChild(option);
+      });
+    })
 });
